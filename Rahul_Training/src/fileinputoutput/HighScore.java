6bf35fd674c4c2f66e2ab9ecf3fb9c2638bee453
tsuser <tsuser@10.1.31.170>
2017-01-09 15:08:56 +0530

    New Program written

---
+++ Rahul_Training/src/fileinputoutput/HighScore.java
@@ -0,0 +1,14 @@
+package fileinputoutput;
+
+import java.util.Scanner;
+
+public class HighScore {
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard=new Scanner(System.in);
+		
+
+	}
+
+}
