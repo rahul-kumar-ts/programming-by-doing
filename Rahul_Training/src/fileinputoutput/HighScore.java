ae73bce52eed20ad1f18c75ddfbdeabcc6760a3d
tsuser <tsuser@10.1.31.170>
2017-01-09 15:40:12 +0530

    New Program added

--- Rahul_Training/src/fileinputoutput/HighScore.java
+++ Rahul_Training/src/fileinputoutput/HighScore.java
@@ -1,14 +1,33 @@
 package fileinputoutput;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class HighScore {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		int score;
+		String name;
 		Scanner keyboard=new Scanner(System.in);
 		
-
+		System.out.print("Enter your name: ");
+		name=keyboard.next();
+		System.out.println("\nEnter your score: ");
+		score=keyboard.nextInt();
+		
+		FileWriter writer=new FileWriter("d:/name.txt");
+		BufferedWriter buffer=new BufferedWriter(writer);
+		
+		buffer.write("Your name is "+name);
+		buffer.newLine();
+		buffer.write("Your score is "+score);
+		
+		buffer.close();
+		
+		System.out.println("Program ended");
 	}
 
 }
